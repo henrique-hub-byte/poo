d98eed28b02b4793034bdb1e2da4dd317cc49d9e
Nadine Gomes Lau da Silva <nadine.lau@flieger.software>
2022-08-19 13:44:50 -0300

    herança,polimorfismo,encapsulamento + anotações 1.1

--- oo_pilar_encapsulamento.php
+++ oo_pilar_encapsulamento.php
@@ -1,74 +1,84 @@
 <?php
-    class Pai  {
-        /* operadores de visibilidade */
-        private $nome = "Luis";
-        protected $sobrenome = "martins";
-        public $humor = 'feliz';
+class Pai
+{
+    /* operadores de visibilidade */
+    private $nome = "Luis";
+    protected $sobrenome = "martins";
+    public $humor = 'feliz';
 
-        public function __get($attr)
-        {
-            return $this->$attr;
-        }
-
-        public function __set($attr, $value)
-        {
-            $this->$attr = $value;
-
-        }
-
-        private function exexutarMania(){
-            echo "assobiar";
-        }
-
-        protected function responder (){
-            echo "oi";
-        }
-
-        public function exercutarAcao(){
-            $this->exexutarMania();
-            echo '<br>';
-            $this->responder();
-        }
+    public function __get($attr)
+    {
+        return $this->$attr;
     }
 
-    class Filho extends pai {
-        /* public function getAtributo($attr){
+    public function __set($attr, $value)
+    {
+        $this->$attr = $value;
+    }
+
+    private function exexutarMania()
+    {
+        echo "assobiar";
+    }
+
+    protected function responder()
+    {
+        echo "oi";
+    }
+
+    public function exercutarAcao()
+    {
+        $this->exexutarMania();
+        echo '<br>';
+        $this->responder();
+    }
+}
+
+class Filho extends pai
+{
+    public function __construct()
+    {
+        /* exibir os metodos do objeto */
+        echo '<pre>';
+        print_r(get_class_methods($this));
+        /* print_r($filhos); */
+        echo '</pre>';
+    }
+    protected function responder()
+    {
+        echo "hello";
+    }
+
+    /* public function getAtributo($attr){
             return $this->$attr;
         } */
-/*
+    /*
         public function setAtributo($attr, $value){
             $this->$attr = $value;
         } */
-    }
+}
 /*
     $pai = new Pai();
     $pai->sobrenome = 'bonfim';
     echo $pai->exercutarAcao(); */
 
-    $filho = new Filho;
-    echo '<pre>';
-    print_r($filho);
-    echo '</pre>';
+$filho = new Filho;
+echo '<pre>';
+print_r($filho);
+echo '</pre>';
 
-    /* metodos da classe filho */
-    /* $filho->setAtributo('nome', 'steven');
+/* metodos da classe filho */
+/* $filho->setAtributo('nome', 'steven');
     echo '<pre>';
     print_r($filho);
     echo '</pre>';
     echo $filho->getAtributo('nome'); */
 
-    /* exibir os metodos do objeto */
-    echo '<pre>';
 
-    print_r(get_class_methods($filho));
-    print_r($filhos);
-    echo '</pre>';
-    echo $filho->
-    $filho->__set('nome', 'outro luis');
-    /* por ter feito o extends do atributo que tem
+echo $filho->__set('nome', 'outro luis');
+/* por ter feito o extends do atributo que tem
     metodos magicos get e set conseguimos alterar os atributos
     private */
-    echo $filho->__get('nome');
-    echo '<hr>';
-    $filho->exercutarAcao();
-?>
+echo $filho->__get('nome');
+echo '<hr>';
+$filho->exercutarAcao();
